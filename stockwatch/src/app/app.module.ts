0cc12bea837f4a80b1d747f5e05b80733acd0a9e
vnpugh <vnp19@yahoo.com>
2023-06-05 17:12:19 -0500

    Updating Routing

--- stockwatch/src/app/app.module.ts
+++ stockwatch/src/app/app.module.ts
@@ -1,23 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-// import { LoginService } from './services/login.service';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
-import { LoginPageComponent } from './login-page/login-page.component';
+
 
 import { RegisterPageComponent } from './register-page/register-page.component';
-// import { DashboardComponent } from './dashboard/dashboard.component';
+
 import { HttpClientModule } from '@angular/common/http';
 import { HomePageComponent } from './home-page/home-page.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LogoComponent } from './logo/logo.component';
 
-// import { AppRoutingModule } from './app-routing.module';
+//import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent,
+    HomePageComponent,
     RegisterPageComponent,
     HomePageComponent,
     NavbarComponent,
