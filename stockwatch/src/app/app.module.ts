cfc35e13664937526dfa1878f6937943ca4aafe4
vnpugh <vnp19@yahoo.com>
2023-06-04 22:59:01 -0500

    Updated Login-Page Component

--- stockwatch/src/app/app.module.ts
+++ stockwatch/src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { LoginService } from './services/login.service';
 
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+
 import { RegisterPageComponent } from './register-page/register-page.component';
+// import { DashboardComponent } from './dashboard/dashboard.component';
+import { HttpClientModule } from '@angular/common/http';
+
+// import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
