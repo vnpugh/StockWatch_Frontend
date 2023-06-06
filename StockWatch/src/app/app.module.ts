e9ae698d76a08df8423db9e811684eaf56400096
vnpugh <vnp19@yahoo.com>
2023-06-05 22:44:12 -0500

    created watchlist component

--- StockWatch/src/app/app.module.ts
+++ StockWatch/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { WatchlistsComponent } from './watchlists/watchlists.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    WatchlistsComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: HomepageComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ StockWatch/src/app/watchlists/watchlists.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-watchlists',
+  templateUrl: './watchlists.component.html',
+  styleUrls: ['./watchlists.component.css']
+})
+export class WatchlistsComponent {
+
+}
