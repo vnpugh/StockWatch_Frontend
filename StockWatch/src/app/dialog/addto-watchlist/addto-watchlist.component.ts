2550aeb70d3666487a43fc26d7479c7b985562b8
vnpugh <vnp19@yahoo.com>
2023-06-08 09:45:27 -0500

    Doctrings for addToWatchList component

--- StockWatch/src/app/dialog/addto-watchlist/addto-watchlist.component.ts
+++ StockWatch/src/app/dialog/addto-watchlist/addto-watchlist.component.ts
@@ -13,16 +13,27 @@ import { AddToWatchlist } from 'src/app/models/add-to-watchlist.request';
   templateUrl: './addto-watchlist.component.html',
   styleUrls: ['./addto-watchlist.component.css']
 })
+
+/**
+* AddtoWatchListComponent has three properties: listId, error, & watchlists.
+* Referencing the dialog component to perform actions relating to the dialog.
+* The user can cancel or add to their watchlist when the buttons are clicked (binding).
+*/ 
 export class AddtoWatchlistComponent {
   listId:number = -1;
   error:string = ''
   watchlists: WatchListResponse[] = []
   constructor(
     public dialogRef: MatDialogRef<HomeGridComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: Stock[], private httpService: HttpService
+    @Inject(MAT_DIALOG_DATA) public data: Stock[], private httpService: HttpService //make requests to the server
   ) {
     this.fetchWatchlists();
   }
+
+  /**
+* Using FetchWatchLists to retrieve user created watchlists from the server (asynchronous ).
+* Error is thrown if the watchlist cannot be retrieved.
+*/ 
 
   fetchWatchlists(): void {
     this.httpService.get('api/watchlist').subscribe({
@@ -40,12 +51,17 @@ export class AddtoWatchlistComponent {
     this.dialogRef.close();
   }
 
+    /**
+* Using addToWatchlist method to represent the logic to add stocks to a watchlist.
+* Subscribe function handles the responses (next & error).
+* Error is thrown if the watchlist cannot be saved.
+*/ 
   addToWatchlist(): void {
     let stockSymbols = this.data?.map(stock=>stock.symbol)
     let addToWatchlistReq: AddToWatchlist = {watchListId: this.listId, stockSymbols: stockSymbols};
     this.httpService.post('api/watchlist/addStocks', addToWatchlistReq).subscribe({
       next: (res: CreateWatchlistResponse) => {
-        this.error = '';
+        this.error = '';   //executed when the request is successful
         this.dialogRef.close('success');
       },
       error: (e) => {
