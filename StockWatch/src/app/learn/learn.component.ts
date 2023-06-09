4623c3fa21d3632c8692e3b11e0f1940a855256d
vnpugh <vnp19@yahoo.com>
2023-06-08 23:16:03 -0500

    Updated learn.ts with terms and modified table

--- StockWatch/src/app/learn/learn.component.ts
+++ StockWatch/src/app/learn/learn.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpService } from '../services/http.service';
 import { Router } from '@angular/router';
+import { Learn } from '../models/learn';
 
 
 @Component({
@@ -8,13 +9,42 @@ import { Router } from '@angular/router';
   templateUrl: './learn.component.html',
   styleUrls: ['./learn.component.css']
 })
+
 export class LearnComponent {
+  displayedColumns: string[] = ['Term', 'Definition'];
+  dataSource: Learn[] = terms;
 
+  constructor(private router: Router) {} 
 
-  constructor(private router: Router) {}
-  
   navigateToLearnPage() {
     this.router.navigate(['/learn']);
   }
-
 }
+
+  const terms: Learn[] = [
+    {
+      Term: 'Aggregate',
+      Definition: 'some term',
+    },
+    {
+      Term: 'Aggregate',
+      Definition: 'some term',
+    },
+    {
+      Term: 'Aggregate',
+      Definition: 'some term',
+    },
+    {
+      Term: 'Aggregate',
+      Definition: 'some term',
+    },
+    {
+      Term: 'Aggregate',
+      Definition: 'some term',
+    },
+  ];
+
+
+
+
+
